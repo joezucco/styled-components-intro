2dc87500a3fda7dfaae5174afbb6b0b6d2839aaf
Joe Zucco <josephzucco@gmail.com>
2021-11-04 12:02:43 -0400

    import theme provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+
 function App() {
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <>
+        <Container>
+          <Header />
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader bg="red">
+      <header>
+        <h1>Hubble</h1>
+      </header>
+    </StyledHeader>
+  );
+};
+
+export default Header;
